96da0e35dc821c4fd701658e81c93a7aa90fc021
Hailee Doyle <haileedoyle@Hailees-MacBook-Pro.local>
2020-07-12 22:50:40 -0500

    updated controller and completed model

--- controllers/control.php
+++ controllers/control.php
@@ -9,16 +9,16 @@ include_once __DIR__ . '/../models/model.php';
     $request_body = file_get_contents('php://input');
     $body_object = json_decode($request_body);
 
-    $new_content = new Kelc($body_object->name, $body_object->image, $body_object->description);
-    $all_content = Kelc::create($new_content);
+    $new_item = new Kelc($body_object->name, $body_object->image, $body_object->description);
+    $all_content = Kelc::create($new_item);
 
     echo json_encode($all_content);
   } else if ($_REQUEST['action'] === 'update'){
     $request_body = file_get_contents('php://input');
     $body_object = json_decode($request_body);
 
-    $updated_content = new Kelc($_REQUEST["id"], $body_object->name, $body_object->image, $body_object->description);
-    $all_content = Kelc::update($updated_content);
+    $updated_item = new Kelc($_REQUEST["id"], $body_object->name, $body_object->image, $body_object->description);
+    $all_content = Kelc::update($updated_item);
 
     echo json_encode($all_content);
   } else if ($_REQUEST['action'] === 'delete') {
---
+++ models/model.php
@@ -0,0 +1,68 @@
+<?php
+$dbconn = pg_connect('host=localhost dbname=kelc');
+
+class Item {
+  public $id;
+  public $name;
+  public $image;
+  public $description;
+
+  public function __construct($id, $name, $image, $description) {
+    $this->id = $id;
+    $this->name = $name;
+    $this->image = $image;
+    $this->description = $description;
+  }
+}
+
+class Content {
+  static function delete($id) {
+    $query = "DELETE FROM content WHERE id = $1";
+    $query_params = array($id);
+    pg_query_params($query, $query_params);
+
+    return self::all();
+  }
+  static function update($updated_item) {
+    $query = "UPDATE content SET name = $1, image = $2, description = $3 WHERE id = $4";
+    $query_params = array($updated_item->name, $updated_item->image, $updated_item->description, $updated_item->id);
+    pg_query_params($query, $query_params);
+
+    return self::all();
+  }
+  static function create($item) {
+    $query = "INSERT INTO content (name, image, description) VALUES ($1, $2, $3)";
+    $query_params = array($item->name, $item->image, $item->description);
+    pg_query_params($query, $query_params);
+
+    return self::all();
+  }
+  static function all() {
+    $content = array();
+
+    $results = pg_query("SELECT * FROM content");
+
+    $row_object = pg_fetch_object($results);
+    while($row_object !== false) {
+
+      $new_item = new Item(
+        intval($row_object->id),
+        $row_object->name,
+        $row_object->image,
+        $row_object->description
+      );
+
+      content[] = $new_item;
+
+      $row_object = pg_fetch_object($results);
+
+    }
+
+    return $content;
+
+  }
+
+}
+
+
+ ?>
