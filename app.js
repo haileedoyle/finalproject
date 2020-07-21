fbbe0ef6b1be50317fc45c9edd7ae27e2393b7a2
Hailee Doyle <haileedoyle@Hailees-MacBook-Pro.local>
2020-07-20 20:03:24 -0500

    css finalish

--- app.js
+++ app.js
@@ -118,29 +118,54 @@ class App extends React.Component {
             </form>
       </nav>
       <div className="about" id="about">
-        <img src="https://i.imgur.com/wyyKCwn.jpg" alt="kelc"/>
-        <p>
-          Kelsey is a Creative Stylist originally from Chicago, Illinois. She is heavily involved in fashion, tattoos, and the make up world. <br/> She is currently attending the Steven Papageorge Hair Academy in Chicago, and graduates in September 2020.
-        </p>
+          <div className="contact" id="contact">
+          <img src="https://i.imgur.com/wyyKCwn.jpg" alt="kelc"/>
+          <h5>Reach Out</h5>
+            <form action="#" method="post">
+              <div className="name">
+                <label for="name"></label>
+                <input type="text" placeholder="Name"/>
+              </div>
+              <div className="email">
+                <label for="email"></label>
+                <input type="email" placeholder="Email"/>
+              </div>
+              <div className="telephone">
+                <label for="name"></label>
+                <input type="text" placeholder="Number"/>
+              </div>
+              <div className="subject">
+                <label for="subject"></label>
+              </div>
+              <div className="message">
+                <label for="message"></label>
+                <textarea name="message" placeholder="I'd like to chat about"></textarea>
+              </div>
+              <div className="submit">
+                <input type="submit" value="Send Message"
+                className="btn btn-dark" />
+              </div>
+            </form>
+          </div>
+        <ul className="styles">
+          {
+            this.state.styles.map(
+              (style) => {
+                return <li className="list-unstyled">
+                  <img src={style.image}/>
+                  <form onSubmit={this.updateStyle} id={style.id} className="form">
+                    <input onKeyUp={this.changeUpdateStyleName} type="text" placeholder="Name"/><br/>
+                    <input onKeyUp={this.changeUpdateStyleImage} type="text" placeholder="Image"/><br/>
+                    <input onKeyUp={this.changeUpdateStyleDesc} type="text" placeholder="Description"/><br/>
+                    <input type="submit" value="update" className="btn btn-dark"/><br/>
+                    <button onClick={this.deleteStyle} value={style.id} className="btn btn-dark">delete</button>
+                  </form>
+                </li>
+              }
+            )
+          }
+        </ul>
       </div>
-      <ul className="styles">
-        {
-          this.state.styles.map(
-            (style) => {
-              return <li className="list-unstyled">
-                <img src={style.image}/>
-                <form onSubmit={this.updateStyle} id={style.id} className="form">
-                  <input onKeyUp={this.changeUpdateStyleName} type="text" placeholder="Name"/><br/>
-                  <input onKeyUp={this.changeUpdateStyleImage} type="text" placeholder="Image"/><br/>
-                  <input onKeyUp={this.changeUpdateStyleDesc} type="text" placeholder="Description"/><br/>
-                  <input type="submit" value="update" className="btn btn-dark"/><br/>
-                  <button onClick={this.deleteStyle} value={style.id} className="btn btn-dark">delete</button>
-                </form>
-              </li>
-            }
-          )
-        }
-      </ul>
     <footer>
       <em>Hailee Doyle 2020</em>
     </footer>
